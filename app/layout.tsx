a2fac6f481d1c2c0bf386eec352b9c3f18855bf3
ewunetiea <ewunetiea@awashbank.com>
2024-03-01 09:11:20 +0300

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,25 @@
+// import { inter } from '@/app/ui/fonts';
+// import '@/app/ui/global.css';
+
+ 
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <body className={`${inter.className} antialiased`}>{children}</body>
+
+//     </html>
+//   );
+// }
+
+
+
+import { inter } from '@/app/ui/fonts';
+import '@/app/ui/global.css';
+ 
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +27,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
