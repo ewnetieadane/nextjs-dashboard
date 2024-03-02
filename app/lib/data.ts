51ed110b529b229dbd07b46f6321b04205de5e7f
ewunetiea <ewunetiea@awashbank.com>
2024-03-02 12:19:03 +0300

    capter complation

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -160,6 +160,7 @@ export async function fetchInvoiceById(id: string) {
   noStore();
 
   try {
+
     const data = await sql<InvoiceForm>`
       SELECT
         invoices.id,
@@ -175,6 +176,7 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
+    
 
     return invoice[0];
   } catch (error) {
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import { updateInvoice } from '@/app/lib/actions';
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
+import { Button } from '@/app/ui/button';
 import {
   CheckIcon,
   ClockIcon,
@@ -8,7 +10,8 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { Button } from '@/app/ui/button';
+import { useFormState } from 'react-dom';
+
 
 export default function EditInvoiceForm({
   invoice,
@@ -17,9 +20,13 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState = { message: null, errors: {} };
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, dispatch] = useFormState(updateInvoiceWithId, initialState);
+
   return (
-    <form>
-      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+    <form action={dispatch}>
+      <input type="hidden" name="id" value={invoice.id} />      <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
